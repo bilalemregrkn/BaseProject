79c067cb86a0ad1f9fd0ea157c6cbdf1a81ec6cd
Bilal Emre Gürkan <emre.gurkan@rovio.com>
2026-04-24 17:24:37 +0300

    feat: add dynamic panel ID retrieval in TestPanelService for improved editor integration

--- Assets/!_Project/Backend/PanelService/TestPanelService.cs
+++ Assets/!_Project/Backend/PanelService/TestPanelService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Reflex.Attributes;
 using Sirenix.OdinInspector;
@@ -9,7 +10,25 @@ namespace Plugins.PanelService
     {
         [Inject] private IPanelService _service;
 
+        [ValueDropdown(nameof(GetPanelIds))]
         public string id;
+
+#if UNITY_EDITOR
+        private static IEnumerable<string> GetPanelIds()
+        {
+            var settings = UnityEditor.AssetDatabase.FindAssets("t:PanelServiceSettings");
+            foreach (var guid in settings)
+            {
+                var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+                var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<PanelServiceSettings>(path);
+                if (asset == null) continue;
+                foreach (var panel in asset.Panels)
+                    if (!string.IsNullOrEmpty(panel.Id))
+                        yield return panel.Id;
+                yield break;
+            }
+        }
+#endif
 
         [Button]
         public void Show()
