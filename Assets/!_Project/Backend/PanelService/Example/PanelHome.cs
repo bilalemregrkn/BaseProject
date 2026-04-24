069a4b48a45112db5f29192e9d108d4c214c3ff1
Bilal Emre Gürkan <emre.gurkan@rovio.com>
2026-04-24 12:49:21 +0300

    refactor: remove unused panel service registration and button click handling from PanelHome

--- Assets/!_Project/Backend/PanelService/Example/PanelHome.cs
+++ Assets/!_Project/Backend/PanelService/Example/PanelHome.cs
@@ -8,24 +8,5 @@ namespace Plugins.PanelService
     public class PanelHome : BasePanel
     {
         [SerializeField] private BaseButton startButton;
-
-        [Inject] private IPanelService _panelService;
-
-        private void Start()
-        {
-            _panelService.Register(PanelType.Home, this);
-            startButton.Clicked += OnStartClicked;
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-            _panelService?.Unregister(PanelType.Home);
-        }
-
-        private void OnStartClicked()
-        {
-            _panelService.ShowExclusiveAsync(PanelType.Game).Forget();
-        }
     }
 }
