e000608c480fbae37585a4c143419efe82030767
Bilal Emre Gürkan <emre.gurkan@rovio.com>
2026-05-04 11:28:29 +0300

    feat: rename textTimer to textCountDown and update Timer class for improved countdown functionality

--- Assets/!_Project/Backend/TimerSystem/Service/TextCountDown.cs
+++ Assets/!_Project/Backend/TimerSystem/Service/TextCountDown.cs
@@ -1,5 +1,6 @@
 using Backend.Systems.Component;
 using Backend.Systems.Update;
+using Reflex.Attributes;
 
 namespace Backend.Systems.Timer
 {
@@ -7,9 +8,24 @@ namespace Backend.Systems.Timer
     {
         private Deadline _deadline;
 
+        [Inject] private IUpdateService _updateService;
+
         public void Init(Deadline deadline)
         {
             _deadline = deadline;
+            _updateService.Add(this);
+        }
+
+        public void Stop()
+        {
+            _deadline = null;
+            _updateService.Remove(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            _updateService?.Remove(this);
         }
 
         public void Tick(float dt)
@@ -17,6 +33,7 @@ namespace Backend.Systems.Timer
             if (_deadline == null) return;
             var ts = _deadline.GetRemainTime();
             SetText($"{(int)ts.TotalMinutes:00}:{ts.Seconds:00}");
+            if (_deadline.IsExpired) Stop();
         }
     }
 }
